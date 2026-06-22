5cb15d1ce86a84fa9ae5fcf1ec477fef0517c81a
menasty <mweinhold86@gmail.com>
2026-06-22 04:29:42 +0000

    chore: non-functional server.js comment cleanup

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Set up EJS
+// Configure EJS templates
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
